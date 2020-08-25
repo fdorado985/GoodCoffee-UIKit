7493cce71b8dc14d43995b0e909d30c0371a92aa
Juan Dorado <fdorado985@gmail.com>
2020-08-25 12:55:25 -0500

    Initial commit and setup project

---
+++ GoodCoffee/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GoodCoffee
+//
+//  Created by Juan Francisco Dorado Torres on 25/08/20.
+//  Copyright © 2020 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
