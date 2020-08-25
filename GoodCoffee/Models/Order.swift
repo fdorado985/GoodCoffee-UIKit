3000e8bb43f5eeed8c0a9e7e15cf5a3b1c30078f
Juan Dorado <fdorado985@gmail.com>
2020-08-25 13:20:55 -0500

    create the models to get the orders

---
+++ GoodCoffee/Models/Order.swift
@@ -0,0 +1,30 @@
+//
+//  Order.swift
+//  GoodCoffee
+//
+//  Created by Juan Francisco Dorado Torres on 25/08/20.
+//  Copyright © 2020 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+  case cappuccino
+  case latte
+  case espressino
+  case cortado
+}
+
+enum CoffeeSize: String, Codable {
+  case small
+  case medium
+  case large
+}
+
+struct Order: Codable {
+
+  let name: String
+  let email: String
+  let type: CoffeeType
+  let size: CoffeeSize
+}
