3cda67be37992692719a88c09222c146e1acadd4
Juan Dorado <fdorado985@gmail.com>
2020-08-25 13:01:45 -0500

    Create the User Interface

---
+++ GoodCoffee/Controllers/OrdersViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersViewController.swift
+//  GoodCoffee
+//
+//  Created by Juan Francisco Dorado Torres on 25/08/20.
+//  Copyright © 2020 Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import UIKit
+
+class OrdersViewController: UITableViewController {
+  
+}
